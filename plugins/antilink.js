c99ca946ae3b2b72b5a90964242dddd6af53c83a
Jhon <165972049+JhonClD@users.noreply.github.com>
2026-05-08 16:52:27 -0500

    Refactor antilink handler to use ES6 export

--- plugins/antilink.js
+++ plugins/antilink.js
@@ -1,6 +1,6 @@
 const linkRgx = /https?:\/\/(chat\.whatsapp\.com|t\.me|telegram\.me|vm\.tiktok\.com|tiktok\.com|youtube\.com|youtu\.be|facebook\.com|instagram\.com|x\.com|twitter\.com)\/\S+/gi
 
-let handler = async (m, { conn, isAdmin, isOwner, isBotAdmin }) => {
+const handler = async (m, { conn, isAdmin, isOwner, isBotAdmin }) => {
   let chat = global.db.data.chats[m.chat]
   if (!chat) global.db.data.chats[m.chat] = chat = {}
   if (/^antilink( (on|off))?$/i.test(m.text?.trim())) {
@@ -19,6 +19,8 @@ let handler = async (m, { conn, isAdmin, isOwner, isBotAdmin }) => {
     mentions: [m.sender]
   })
 }
+
 handler.all = true
 handler.group = true
-module.exports = handler
+
+export default handler
